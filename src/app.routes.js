eb4f98be85fe2fce2244f46d4e118e9af8f735a6
erfan sharafi <erfansharafi60@gmail.com>
2025-06-05 16:05:40 +0330

    feat(payment): add payment routes and integrate into main router
    Introduce payment routes for handling payment processing. The new payment module includes a POST endpoint protected by authentication guard. This update enhances the application by allowing users to manage payment transactions effectively.

--- src/app.routes.js
+++ src/app.routes.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const { productRoutes } = require("./modules/product/product.routes");
 const { authRoutes } = require("./modules/auth/auth.routes");
 const { basketRoutes } = require("./modules/basket/basket.routes");
+const { paymentRoutes } = require("./modules/payment/payment.routes");
 
 const mainRouter = Router();
 
 mainRouter.use("/product", productRoutes);
 mainRouter.use("/auth", authRoutes);
 mainRouter.use("/basket", basketRoutes);
+mainRouter.use("/payment", paymentRoutes);
 
 module.exports = mainRouter;
